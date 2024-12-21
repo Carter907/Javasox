0e5658bd7f8e549b929e5b51e0a6a2ed86c7c4d1
Carter <speers.carter@gmail.com>
2024-12-21 12:37:43 -0500

    fixed test client location

--- javasox-test-client/index.js
+++ javasox-test-client/index.js
@@ -1,7 +1,7 @@
 import { WebSocket } from 'ws'
 
 let ws = new WebSocket("ws://localhost:8080")
-ws.onopen = ((data: any) => {
+ws.onopen = ((data) => {
     console.log(data);
     console.log("connect")
 });
